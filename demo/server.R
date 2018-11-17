058e4ce38c55f9762b8b7ff6fc63faa0e7f478fa
amadeusz <amadeuszmasny@gmail.com>
2018-11-17 20:57:39 +0100

    clean demo

--- demo/server.R
+++ demo/server.R
@@ -35,31 +35,6 @@ function(input, output, session) {
         longitude >= lngRng[1] & longitude <= lngRng[2])
   })
 
-  # Precalculate the breaks we'll need for the two histograms
-  #centileBreaks <- hist(plot = FALSE, allzips$centile, breaks = 20)$breaks
-
-  # output$histCentile <- renderPlot({
-  #   # If no zipcodes are in view, don't plot
-  #   if (nrow(zipsInBounds()) == 0)
-  #     return(NULL)
-  # 
-  #   hist(zipsInBounds()$centile,
-  #     breaks = centileBreaks,
-  #     main = "SuperZIP score (visible zips)",
-  #     xlab = "Percentile",
-  #     xlim = range(allzips$centile),
-  #     col = '#00DD00',
-  #     border = 'white')
-  # })
-
-  # output$scatterCollegeIncome <- renderPlot({
-  #   # If no zipcodes are in view, don't plot
-  #   if (nrow(zipsInBounds()) == 0)
-  #     return(NULL)
-  # 
-  #   print(xyplot(income ~ college, data = zipsInBounds(), xlim = range(allzips$college), ylim = range(allzips$income)))
-  # })
-
   # This observer is responsible for maintaining the circles and legend,
   # according to the variables the user has chosen to map to color and size.
   observe({
