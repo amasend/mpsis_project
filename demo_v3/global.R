0af4b40a2dc358f3076332a095b74b939d81ed44
amadeusz <amadeuszmasny@gmail.com>
2018-12-03 02:42:42 +0100

    demo_v3

---
+++ demo_v3/global.R
@@ -0,0 +1,9 @@
+library(dplyr)
+
+#zipdata <- read.csv("data/flights.csv")
+zipdata <- read.csv("data/dane.csv", sep=";", col.names = c("From", "To", "Year", "Month", "Day", "Time", "Price", "From_latitude", "From_longitude",
+                                                            "To_latitude", "To_longitude", "name"))
+cityFrom <- read.csv("data/cityFrom.csv")
+IANA_codes <- read.csv("IANA_codes_coordinates.csv")
+#zzz <- read.csv("data/dane.csv", sep=";", col.names = c("From", "To", "Date", "Price"))
+airport_names <- unique(zipdata$name)
